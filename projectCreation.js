6d8ab0280df42586be93010ebf03ecfeb6ad83ff
AndreyDimitrov89 <andrey89dimitrov@gmail.com>
2024-09-09 18:09:36 +0300

    Add files via upload

---
+++ projectCreation.js
@@ -0,0 +1,6 @@
+function projectsCreation(architectName, numberOfProjects){
+    let hoursPerProjects = 3;
+    let totalHours = numberOfProjects * hoursPerProjects;
+    console.log ("The architect " + architectName + " will need " + totalHours + " hours to complete " + numberOfProjects + " project/s.")
+}
+projectsCreation("Andrey", 3)
